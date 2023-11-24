f8933693cb973020740d088594bd50cc0430ac40
unknown <you@example.com>
2023-11-24 23:34:37 +0600

    78.8

--- src/Payges/Home/Featured/Featured.jsx
+++ src/Payges/Home/Featured/Featured.jsx
@@ -5,9 +5,9 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white pt-8 my-20'>
+        <div className='featured-item bg-fixed text-white pt-8 my-20'>
             <SectionTitle heading="Featured Item" subHeading="Check It Out"></SectionTitle>
-           <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+           <div className='md:flex justify-center items-center pb-20 pt-12 px-36 bg-opacity-30 bg-slate-300 text-black'>
            <div>
                   <img src={featuredImg} alt="" />
             </div>
@@ -15,7 +15,7 @@ const Featured = () => {
                 <p>November 24, 2023</p>
                 <p className='uppercase'>Where can I get some?</p>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ratione earum alias molestiae blanditiis, quas quidem explicabo magnam atque debitis consectetur modi ut architecto ipsum iure aut eos molestias, rerum nobis dolores aperiam? Quisquam iusto odio totam consectetur culpa eos, saepe sed eligendi similique, maiores fugit, quibusdam ea quis recusandae?</p>
-                <button className='btn btn-outline'>Order Now</button>
+                <button className='btn btn-outline border-0 border-b-4 mt-4'>Order Now</button>
             </div>
            </div>
         </div>
--- src/Payges/Home/Home/Home.jsx
+++ src/Payges/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
 import PopulerMenu from '../PopulerMenu/PopulerMenu';
 import Featured from '../Featured/Featured';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Category></Category>
             <PopulerMenu></PopulerMenu>
             <Featured></Featured>
+            <Testimonials></Testimonials>
         </div>
     );
 };
