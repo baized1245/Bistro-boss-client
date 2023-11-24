440976e8fce2f3ff05f2bee82d3df188a023bb38
unknown <you@example.com>
2023-11-24 22:51:50 +0600

    74.7

---
+++ src/Payges/Home/Featured/Featured.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import featuredImg from '../../../assets/home/featured.jpg'
+import './Featured.css'
+
+const Featured = () => {
+    return (
+        <div className='featured-item text-white pt-8 my-20'>
+            <SectionTitle heading="Featured Item" subHeading="Check It Out"></SectionTitle>
+           <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+           <div>
+                  <img src={featuredImg} alt="" />
+            </div>
+            <div className='md:ml-10'>
+                <p>November 24, 2023</p>
+                <p className='uppercase'>Where can I get some?</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ratione earum alias molestiae blanditiis, quas quidem explicabo magnam atque debitis consectetur modi ut architecto ipsum iure aut eos molestias, rerum nobis dolores aperiam? Quisquam iusto odio totam consectetur culpa eos, saepe sed eligendi similique, maiores fugit, quibusdam ea quis recusandae?</p>
+                <button className='btn btn-outline'>Order Now</button>
+            </div>
+           </div>
+        </div>
+    );
+};
+
+export default Featured;
