7e091fe0f6219e8c36992b36d8f19309c17e82ea
unknown <you@example.com>
2023-11-25 14:59:04 +0600

    75.2

--- src/Payges/Menu/Menu/Menu.jsx
+++ src/Payges/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet'
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/dessert-bg.jpeg'
+import PopulerMenu from '../../Home/PopulerMenu/PopulerMenu';
 
 const Menu = () => {
     return (
@@ -7,7 +10,8 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h3>Menu section</h3>
+            <Cover img={menuImg} title={"Our Menu"}></Cover>
+            <PopulerMenu></PopulerMenu>   
         </div>
     );
 };
