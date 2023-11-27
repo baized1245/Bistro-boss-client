8670d28570f71719feec298bc53cef1273d0b550
unknown <you@example.com>
2023-11-27 15:52:56 +0600

    77.8

--- src/Payges/Dashboard/MyCart/MyCart.jsx
+++ src/Payges/Dashboard/MyCart/MyCart.jsx
@@ -1,21 +1,62 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import useCart from '../../../Hooks/useCart';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const MyCart = () => {
     const [ cart ] = useCart();
-    console.log(cart);
+    // console.log(cart);
     const total = cart.reduce((sum, item) => item.price + sum, 0)
     return (
         <div>
             <Helmet>
                 <title>Bisro Boss | My Cart</title>
             </Helmet>
-            <div className='uppercase'>
-                <h3 className='text-3xl'>Items: {cart.length}</h3>
-                <h3 className='text-3xl'>Price: ${total}</h3>
+            <div className='uppercase font-semibold h-[60px] flex justify-evenly items-center'>
+                <h3 className='text-3xl'>Total Items: {cart.length}</h3>
+                <h3 className='text-3xl'>Total Price: ${total}</h3>
                 <button className='btn btn-warning btn-sm'>Pay</button>
             </div>
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Food</th>
+        <th>Item Name</th>
+        <th>Price</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        cart.map((item, index) => <tr 
+        key={item._id}
+        >
+            <td>
+              {index + 1}
+            </td>
+            <td>
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+            </td>
+            <td>
+            {item.name}
+            </td>
+            <td className='text-end'>${item.price}</td>
+            <td>
+              <button className="btn text-white bg-red-600"><FaTrashAlt></FaTrashAlt></button>
+            </td>
+          </tr>)
+      }
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -41,7 +41,7 @@ const AuthProviders = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log("current user", currentUser);
+            // console.log("current user", currentUser);
             setLoading(false)
         });
         return () => {
