0ee5270ac7323a0ff6864d3af539d933f87f9f86
unknown <you@example.com>
2023-11-28 15:01:29 +0600

    78.5

--- src/Payges/Dashboard/AllUsers/AllUsers.jsx
+++ src/Payges/Dashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import { FaTrashAlt, FaUserShield } from 'react-icons/fa';
 
 const AllUsers = () => {
-    // const { data: users = [], refetch } = useQuery(['users'], async() => {
-    //     const res = await fetch('http://localhost:5000/users')
-    //     return res.json();
-    // })
 
     const { refetch, data: users = [] } = useQuery({
         queryKey: ['users'],
@@ -13,11 +11,51 @@ const AllUsers = () => {
             const res = await fetch('http://localhost:5000/users')
             return res.json();  
         },
-      })
+      });
+
+      const handleMakeAdmin = id => {
+        
+      }
+
+      const handleDelete = user => {
+
+      }
 
     return (
-        <div>
-            <h3>{users.length}</h3>
+        <div className='w-full ml-8'>
+            <Helmet>
+                <title>Bistro Boss | All Users</title>
+            </Helmet>
+            <h3 className='text-semibold text-3xl my-4'>Total Users: {users.length}</h3>
+
+            <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        users.map((user, index) => <tr
+        key={user._id}
+        >
+        <th>{index + 1}</th>
+        <td>{user?.name}</td>
+        <td>{user?.email}</td>
+        <td>{ user?.role === 'admin' ? 'admin' : <button onClick={() =>handleMakeAdmin(user?._id)} className="btn text-white bg-orange-500"><FaUserShield></FaUserShield></button> }</td>
+        <td><button onClick={() =>handleDelete(user)} className="btn text-white bg-red-600"><FaTrashAlt></FaTrashAlt></button></td>
+      </tr>)
+      }
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
